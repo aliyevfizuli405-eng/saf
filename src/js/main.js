0bc53184b17cbbaec30c4d2335e64d53c3c1cd3a
aliyevfizuli405-eng <aliyevfizuli405@gmail.com>
2026-09-25 19:57:27 +0400

    Adding Herobg blackoverlay for better text view fixing nav and position problems chaning variabla names

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,80 @@
+
 const $ = (id) => document.getElementById(id);
 
-let menuItemsElement = $("menu__items");
-let user__commentsElement = $("user__comments");
-let MenuBtnElement = $("MenuBtn");
-let MenuListElement = $("MenuList");
-let Menuchecker = false;
-let AntiPastiElemenet = $("AntiPasti");
-let PrimiElement=$("Primi");
-let SecondiElement=$("Secondi");
-let DolciElement=$("Dolci");
-let employeerElemenet=$("employeer");
-let galleryPhotosElement=$("galleryPhotos");
-let allBtnElement=$("all");
-let interiorElement=$("interior");
-let foodElement=$("food");
-let drinksELement=$("drinks");
+const menuItemsElement = $("menu__items");
+const userCommentsElement = $("user__comments");
+const menuBtnElement = $("MenuBtn");
+const menuListElement = $("MenuList");
+let menuChecker = false;
+const antiPastiElemenet = $("AntiPasti");
+const primiElement = $("Primi");
+const secondiElement = $("Secondi");
+const dolciElement = $("Dolci");
+const employeerElemenet = $("employeer");
+const galleryPhotosElement = $("galleryPhotos");
+const allBtnElement = $("all");
+const interiorElement = $("interior");
+const foodElement = $("food");
+const drinksELement = $("drinks");
+const formNameElement=$("formName");
+const formEmailElement=$("formEmail");
+const formDateElement=$("formDate");
+const formTimeELement=$("formTimeElement");
+const formGuestCountElemenet=$("formGuestCount");
+const reservationBtnElement=$("reservationBtn");
 
+
+
+// Functions
 async function GetItems(link) {
     const res = await fetch(link);
     const data = await res.json();
     return data;
+}
+
+async function RenderAntiPasti(link, category, element) {
+    const AntiPasti = await GetItems(link);
+
+    for (let i = 0; i < AntiPasti.length; ++i) {
+        if (AntiPasti[i].category == category) {
+            element.innerHTML += `
+         <div class="foodCard">
+                        <span class="foodCardSeperator">
+                            <h3 class="foodCardTitle">${AntiPasti[i].name}</h3>
+                            <span class="foodCardPrice">${AntiPasti[i].price}</span>
+                        </span>
+                        <p class="foodCardDesc">${AntiPasti[i].description}</p>
+                    </div>
+        `
+        }
+    }
+}
+
+
+async function RenderGalleryPhotos(link, category) {
+    galleryPhotosElement.innerHTML = ``;
+    let photos = await GetItems(link);
+    const temp = photos.filter(photo => photo.category.includes(category))
+    for (let i = 0; i < temp.length; ++i) {
+        galleryPhotosElement.innerHTML += `<img class="galleryPhoto" src="${temp[i].imageUrl}">`
+    }
+}
+
+async function RenderEmplooyers(link) {
+    const employeer = await GetItems(link);
+
+    for (let i = 0; i < employeer.length; ++i) {
+        employeerElemenet.innerHTML += `
+         <div class="employeer__card">
+                        <img src="${employeer[i].imageUrl}" style="width:100%;border-top-left-radius:20px;border-top-right-radius:20px;" alt="">
+                        <div class="employeer__info">
+                            <h3 class=foodCardTitle>${employeer[i].name}</h3>
+                            <h6 class="subtitle">${employeer[i].position}</h6>
+                            <p class="foodCardDesc">${employeer[i].desc}</p>
+                        </div>
+                    </div>
+        `
+    }
 }
 
 async function RenderMenuItems(link) {
@@ -43,12 +98,10 @@ async function RenderMenuItems(link) {
     }
 }
 
-RenderMenuItems("http://localhost:3000/foods");
-
 async function RenderTestimonials(link) {
     const comments = await GetItems(link);
     for (let i = 0; i < comments.length; ++i) {
-        user__commentsElement.innerHTML +=
+        userCommentsElement.innerHTML +=
             `
         <div class="testimonial__card">
         <span style="display:flex;gap:0.5rem;">
@@ -65,82 +118,45 @@ async function RenderTestimonials(link) {
         `
     }
 }
-RenderTestimonials("http://localhost:3000/testimonials");
 
-MenuBtnElement.addEventListener("click", () => {
-    if (!Menuchecker) {
-        MenuListElement.classList.remove("d-none");
-        MenuBtnElement.innerHTML = `<i class = "gg-close"></i>`
+menuBtnElement.addEventListener("click", () => {
+    if (!menuChecker) {
+        menuListElement.classList.remove("d-none");
+        menuBtnElement.innerHTML = `<i class = "gg-close"></i>`
     }
     else {
-        MenuListElement.classList.add("d-none");
-        MenuBtnElement.innerHTML = `<i class="gg-menu"></i>`;
+        menuListElement.classList.add("d-none");
+        menuBtnElement.innerHTML = `<i class="gg-menu"></i>`;
     }
-    Menuchecker = !Menuchecker;
+    menuChecker = !menuChecker;
 })
 
-async function RenderAntiPasti(link,category,element) {
-    const AntiPasti = await GetItems(link);
+RenderMenuItems("http://localhost:3000/foods");
 
-    for (let i = 0; i < AntiPasti.length; ++i) {
-        if (AntiPasti[i].category == category) {
-            element.innerHTML += `
-         <div class="foodCard">
-                        <span class="foodCardSeperator">
-                            <h3 class="foodCardTitle">${AntiPasti[i].name}</h3>
-                            <span class="foodCardPrice">${AntiPasti[i].price}</span>
-                        </span>
-                        <p class="foodCardDesc">${AntiPasti[i].description}</p>
-                    </div>
-        `
-        }
-    }
-}
+RenderTestimonials("http://localhost:3000/testimonials");
 
-RenderAntiPasti("http://localhost:3000/Menu","Antipasti",AntiPastiElemenet);
-RenderAntiPasti("http://localhost:3000/Menu","Primi",PrimiElement);
-RenderAntiPasti("http://localhost:3000/Menu","Secondi",SecondiElement);
-RenderAntiPasti("http://localhost:3000/Menu","Dolci",DolciElement);
-
-async function RenderEmplooyers(link){
-    const employeer= await GetItems(link);
-
-    for(let i=0;i<employeer.length;++i){
-        employeerElemenet.innerHTML+=`
-         <div class="employeer__card">
-                        <img src="${employeer[i].imageUrl}" style="width:100%;border-top-left-radius:20px;border-top-right-radius:20px;" alt="">
-                        <div class="employeer__info">
-                            <h3 class=foodCardTitle>${employeer[i].name}</h3>
-                            <h6 class="subtitle">${employeer[i].position}</h6>
-                            <p class="foodCardDesc">${employeer[i].desc}</p>
-                        </div>
-                    </div>
-        `
-    }
-}
+RenderAntiPasti("http://localhost:3000/Menu", "Antipasti", antiPastiElemenet);
+RenderAntiPasti("http://localhost:3000/Menu", "Primi", primiElement);
+RenderAntiPasti("http://localhost:3000/Menu", "Secondi", secondiElement);
+RenderAntiPasti("http://localhost:3000/Menu", "Dolci", dolciElement);
 
 RenderEmplooyers("http://localhost:3000/employeers");
 
-async function RenderGalleryPhotos(link,category){
-    galleryPhotosElement.innerHTML=``; 
-    let photos = await GetItems(link);
-    const temp=photos.filter(photo=>photo.category.includes(category))
-    for(let i =0;i<temp.length;++i){
-        galleryPhotosElement.innerHTML+=`<img class="galleryPhoto" src="${temp[i].imageUrl}">`
-    }
-}
+RenderGalleryPhotos("http://localhost:3000/gallery", "All");
 
-RenderGalleryPhotos("http://localhost:3000/gallery","All");
+allBtnElement.addEventListener("click", () => {
+    RenderGalleryPhotos("http://localhost:3000/gallery", "All");
+});
 
-allBtnElement.addEventListener("click",()=>{
-    RenderGalleryPhotos("http://localhost:3000/gallery","All");
+
+interiorElement.addEventListener("click", () => {
+    RenderGalleryPhotos("http://localhost:3000/gallery", "Interior");
 });
-interiorElement.addEventListener("click",()=>{
-    RenderGalleryPhotos("http://localhost:3000/gallery","Interior");
+foodElement.addEventListener("click", () => {
+    RenderGalleryPhotos("http://localhost:3000/gallery", "Food");
 });
-foodElement.addEventListener("click",()=>{
-    RenderGalleryPhotos("http://localhost:3000/gallery","Food");
-});
-drinksELement.addEventListener("click",()=>{
-    RenderGalleryPhotos("http://localhost:3000/gallery","Drinks");
+drinksELement.addEventListener("click", () => {
+    RenderGalleryPhotos("http://localhost:3000/gallery", "Drinks");
 })
+
+
